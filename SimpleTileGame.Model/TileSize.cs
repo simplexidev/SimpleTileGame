d0ad8678ba9f7e3a60e3153429dbb75fc6c99fbe
tom-corwin <tacorwin@outlook.com>
2022-03-31 17:19:53 -0400

    Added a TileSize enum, supporting 16, 32, and64px tiles.

---
+++ SimpleTileGame.Model/TileSize.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleTileGame.Model
+{
+    /// <summary>
+    /// Specifies a predefined tile size.
+    /// </summary>
+    public enum TileSize
+    {
+        /// <summary>
+        /// A 16px tile size.
+        /// </summary>
+        Small = 16,
+
+        /// <summary>
+        /// A 32px tile size.
+        /// </summary>
+        Medium = 32,
+
+        /// <summary>
+        /// A 64px tile size.
+        /// </summary>
+        Large = 64
+    }
+}
